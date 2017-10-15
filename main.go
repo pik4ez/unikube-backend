e6e42079ac45beeefd98aded1973cf911e987093
Dmitry Khasanov <dkhasanov@avito.ru>
2017-10-15 17:50:06 +0200

    local- Local HP storage implemented.

--- main.go
+++ main.go
@@ -26,24 +26,17 @@ var lastAssignedUser int
 // db holds database connection
 var db *sql.DB
 
+// local hp start
+var localHP = make(map[string]int)
+
 // handlerState prints player health
 func handlerState(w http.ResponseWriter, r *http.Request) {
 	playerID := strings.TrimSuffix(strings.TrimPrefix(r.URL.Path, "/state/"), "/")
-	query := fmt.Sprintf(`
-		select hp from players where player_id = '%s'
-	`, playerID)
-	var hp int
-	err := db.QueryRow(query).Scan(&hp)
-	if err != nil {
-		log.Printf(`failed to get user %s hp, query %s, err: %s`, playerID, query, err)
-		fmt.Fprint(w, `{"result": "err", "player_id": ""}`)
-		return
-	}
 	fmt.Fprintf(
 		w,
 		`{"result": "ok", "player": "%s", "hp": %d}`,
 		playerID,
-		hp,
+		localHP[playerID],
 	)
 }
 
@@ -60,42 +53,19 @@ func handlerInit(w http.ResponseWriter, r *http.Request) {
 		} else {
 			playerID = availablePlayerIDs[2]
 		}
-		query := fmt.Sprintf(`
-			delete from players where player_id = '%[1]s';
-			insert into players (player_id, hp) values ('%[1]s', 100);
-		`, playerID)
-		_, err := db.Exec(query)
-		if err != nil {
-			log.Printf(`failed to init player %s, query %s, err: %s`, playerID, query, err)
-			fmt.Fprint(w, `{"result": "err", "player_id": ""}`)
-			return
-		}
+		localHP[playerID] = 100
 		fmt.Fprintf(w, `{"result": "ok", "player_id": "%s"}`, playerID)
 		return
 	}
-	query := fmt.Sprintf(`
-		update players set hp = 100 where player_id = '%s'
-	`, argPlayerID)
-	_, err := db.Exec(query)
-	if err != nil {
-		log.Printf(`failed to restore hp for player %s, query %s, err: %s`, argPlayerID, query, err)
-		fmt.Fprint(w, `{"result": "err", "player_id": ""}`)
-		return
-	}
+	localHP[argPlayerID] = 100
 	fmt.Fprintf(w, `{"result": "ok", "player_id": "%s"}`, argPlayerID)
 }
 
 // handlerDamage commits damage received by user
 func handlerDamage(w http.ResponseWriter, r *http.Request) {
 	playerID := strings.TrimSuffix(strings.TrimPrefix(r.URL.Path, "/damage/"), "/")
-	query := fmt.Sprintf(`
-		update players set hp = hp - 20 where hp > 0 and player_id = '%s'
-	`, playerID)
-	_, err := db.Exec(query)
-	if err != nil {
-		log.Printf(`failed to update hp for player %s, query %s, err: %s`, playerID, query, err)
-		fmt.Fprint(w, `{"result": "err", "player_id": ""}`)
-		return
+	if localHP[playerID] > 0 {
+		localHP[playerID] -= 20
 	}
 	fmt.Fprintf(w, `{"result": "ok", "player_id": "%s"}`, playerID)
 }
